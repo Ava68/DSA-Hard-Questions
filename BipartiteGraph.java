09c009444c02672d1dc35ec2c07bc2acdd64234a
Avadhesh kumar singh <63304698+Ava68@users.noreply.github.com>
2021-09-25 16:20:37 +0530

    Create BipartiteGraph.java

---
+++ BipartiteGraph.java
@@ -0,0 +1,44 @@
+// problem Link -> https://leetcode.com/problems/is-graph-bipartite/
+
+//Algorithm ->> Try to color every node with 2 color if 2 adjcent node color is same then return false
+//and if the full graph colored with 2 color and no two adjacent color are same then return true at end
+    
+
+class Solution {
+    public boolean isBipartite(int[][] graph) { 
+        int color [] = new int [graph.length];
+        for(int i=0; i<graph.length; i++)
+            color[i] = -1;
+        
+        for(int i =0; i<graph.length; i++)
+        {
+            if(color[i] == -1)
+            {
+                if(!dfsVisit(graph,i,color))
+                  return false;
+            }
+                
+        }     
+        return true;
+}
+    public boolean dfsVisit(int graph[][] , int node, int color[])
+    {
+        if(color[node] == -1)
+            color[node] = 1;
+        
+        for(int it: graph[node] )
+        {
+            if(color[it] == -1)
+            {
+                color[it] = 1 - color[node];
+                if(dfsVisit(graph,it,color) == false)
+                    return false;
+                
+            }
+            else if(color[it] == color[node])
+                return false;
+        }
+        return true;
+    }
+    
+}
